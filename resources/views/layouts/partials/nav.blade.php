0a1a782f95e828b3c5c2207e7b080c2f73cd4e66
AquaroTorres <atorresf@gmail.com>
2022-07-07 19:32:37 -0400

    Eliminar menú SAMU

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -481,11 +481,11 @@
 <ul class="nav flex-column">
 
     @canany(['SAMU'])
-    <li class="nav-item">
+    <!--li class="nav-item">
         <a class="nav-link {{ active('samu.welcome') }}" href="{{ route('samu.welcome') }}">
             <i class="fas fa-ambulance"></i> SAMU
         </a>
-    </li>
+    </li-->
     @endcanany
 
 </ul>
