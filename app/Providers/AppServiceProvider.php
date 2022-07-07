03a17cb6bb8258630675edee752996d6175389f7
AquaroTorres <atorresf@gmail.com>
2022-07-07 17:32:55 -0400

    assets with https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -32,5 +32,8 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
+        if(config('app.env') === 'production') {
+            \URL::forceScheme('https');
+        }
     }
 }
