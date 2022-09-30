a2299b155937bfda7da75710864943f10d49beef
tebiccr <tebiccr@gmail.com>
2022-09-30 19:13:54 -0300

    sirmx a siremx

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -199,7 +199,7 @@
 						<a href="https://esmeralda.saludtarapaca.gob.cl" class="btn btn-outline-info btn-block">Esmeralda</a>
 					</li>
 					<li class="list-group-item">
-						<a href="https://i.saludiquique.cl/claveunica?redirect=L3NpcmVteC9sb2dpbmN1" class="btn btn-outline-info btn-block">Sirmx</a>
+						<a href="https://i.saludiquique.cl/claveunica?redirect=L3NpcmVteC9sb2dpbmN1" class="btn btn-outline-info btn-block">Siremx</a>
 					</li>
 
 				</ul>
