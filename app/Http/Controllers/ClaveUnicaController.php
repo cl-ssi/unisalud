8691954fba6a0dc55813c06a98473ed1d1a4a91f
Esteban Miranda <tebiccr@gmail.com>
2023-05-25 09:35:21 -0400

    mensaje si response es null o !access_token

--- app/Http/Controllers/ClaveUnicaController.php
+++ app/Http/Controllers/ClaveUnicaController.php
@@ -13,137 +13,144 @@ use Illuminate\Support\Facades\Log;
 
 class ClaveUnicaController extends Controller
 {
-	/** 
-	 * Está implementada un login alternativo para iOnline
-	 * Una vez que se migre ya no será necesario
-	 */
-	/** iOnline Borrar el utlimo parametro $route (tambien en el web.php) cuando ya no se ocupe la redireccion */ 
-	public function autenticar(Request $request, $route = null){
-		/* Primer paso, redireccionar al login de clave única */
-		$url_base       = "https://accounts.claveunica.gob.cl/openid/authorize/";
-		$client_id      = env("CLAVEUNICA_CLIENT_ID");
+    /** 
+     * Está implementada un login alternativo para iOnline
+     * Una vez que se migre ya no será necesario
+     */
+    /** iOnline Borrar el utlimo parametro $route (tambien en el web.php) cuando ya no se ocupe la redireccion */
+    public function autenticar(Request $request, $route = null)
+    {
+        /* Primer paso, redireccionar al login de clave única */
+        $url_base       = "https://accounts.claveunica.gob.cl/openid/authorize/";
+        $client_id      = env("CLAVEUNICA_CLIENT_ID");
 
-		/** iOnline, descomentar y borrar la otra linea */
-		$redirect_uri   = urlencode(env('APP_URL')."/claveunica/callback?route=$route");
-		// $redirect_uri   = urlencode(env('APP_URL')."/claveunica/callback");
+        /** iOnline, descomentar y borrar la otra linea */
+        $redirect_uri   = urlencode(env('APP_URL') . "/claveunica/callback?route=$route");
+        // $redirect_uri   = urlencode(env('APP_URL')."/claveunica/callback");
 
-		$state 			= csrf_token();
-		$scope      	= 'openid run name';
+        $state             = csrf_token();
+        $scope          = 'openid run name';
 
-		$params     	= '?client_id='.$client_id.
-						'&redirect_uri='.$redirect_uri.
-						'&scope='.$scope.
-						'&response_type=code'.
-						'&state='.$state;
-		
-		return redirect()->to($url_base.$params)->send();
-	}
+        $params         = '?client_id=' . $client_id .
+            '&redirect_uri=' . $redirect_uri .
+            '&scope=' . $scope .
+            '&response_type=code' .
+            '&state=' . $state;
 
-    public function callback(Request $request) {
-		/* Segundo paso, el usuario ya se autentificó correctamente en CU y retornó a nuestro sistema */
+        return redirect()->to($url_base . $params)->send();
+    }
 
-		/* Nos aseguramos que vengan los parámetros desde CU */
-		//		if ($request->missing(['code','name'])) {
-		//			return redirect()->route('welcome');
-		//		}
+    public function callback(Request $request)
+    {
+        /* Segundo paso, el usuario ya se autentificó correctamente en CU y retornó a nuestro sistema */
 
-		/* Recepcionamos los siguientes parametros desde CU */
-		$code   		= $request->input('code');
-		$state  		= $request->input('state');
+        /* Nos aseguramos que vengan los parámetros desde CU */
+        //		if ($request->missing(['code','name'])) {
+        //			return redirect()->route('welcome');
+        //		}
 
-		$url_base       = "https://accounts.claveunica.gob.cl/openid/token/";
-		$client_id      = env("CLAVEUNICA_CLIENT_ID");
-		$client_secret  = env("CLAVEUNICA_SECRET_ID");
-		$redirect_uri   = urlencode(env('APP_URL')."/claveunica/callback");
+        /* Recepcionamos los siguientes parametros desde CU */
+        $code           = $request->input('code');
+        $state          = $request->input('state');
 
-		$scope = 'openid+run+name';
+        $url_base       = "https://accounts.claveunica.gob.cl/openid/token/";
+        $client_id      = env("CLAVEUNICA_CLIENT_ID");
+        $client_secret  = env("CLAVEUNICA_SECRET_ID");
+        $redirect_uri   = urlencode(env('APP_URL') . "/claveunica/callback");
 
-		$response = Http::asForm()->post($url_base, [
-			'client_id'     => $client_id,
-			'client_secret' => $client_secret,
-			'redirect_uri'  => $redirect_uri,
-			'grant_type'    => 'authorization_code',
-			'code'          => $code,
-			'state'         => $state,
-		]);
+        $scope = 'openid+run+name';
+
+        $response = Http::asForm()->post($url_base, [
+            'client_id'     => $client_id,
+            'client_secret' => $client_secret,
+            'redirect_uri'  => $redirect_uri,
+            'grant_type'    => 'authorization_code',
+            'code'          => $code,
+            'state'         => $state,
+        ]);
 
 
-		/** iOnline dejar solo el ultimo return */
-		$route  		= $request->input('route');
+        /** iOnline dejar solo el ultimo return */
+        $route          = $request->input('route');
 
-		if(isset($route))
-		{
-			$redirect = str_ireplace('uni.', $route.'.', parse_url(env('APP_URL'), PHP_URL_HOST));
-			//Log::channel('slack')->info('Logeo por CU desde unisalud a iOnline');
-            
-			return redirect('https://'.$redirect.'/claveunica/login/'.json_decode($response)->access_token);
-		}
-		else
-		{
-			return $this->getUserInfo(json_decode($response)->access_token);
-		}
-	}
+        if (isset($route)) {
+            $redirect = str_ireplace('uni.', $route . '.', parse_url(env('APP_URL'), PHP_URL_HOST));
+            //Log::channel('slack')->info('Logeo por CU desde unisalud a iOnline');
 
-    public function getUserInfo($access_token) {
-		/* Paso 3, obtener los datos del usuario en base al $access_token */
-		$url_base = "https://accounts.claveunica.gob.cl/openid/userinfo/";
-		$response = Http::withToken($access_token)->post($url_base);
+            $responseData = json_decode($response);
 
-		$user_clave_unica = json_decode($response);
+            if ($responseData === null || !property_exists($responseData, 'access_token')) {
+                return redirect()->route('welcome')->with('flash_message', 'No se pudo iniciar Sesión con Clave Única');
+            }
 
-		/* Registrar los datos del usuario en la BD local */
 
-		$user_local = User::query()
-						->getByRun($user_clave_unica->RolUnico->numero)
-						->first();
 
-		if($user_local) {
-			/* Actualiza el correo si es que ha cambiado */
-			if(property_exists($user_clave_unica,'email')) {
-				if($user_local->email != $user_clave_unica->email) {
-					$user_local->email = $user_clave_unica->email;
-					$user_local->save();
-				}
-			}
-		} 
-		else {
-			$user_local = new User();
-			$user_local->active = 1;
-			$user_local->claveunica = true;
-			$user_local->save();
+            return redirect('https://' . $redirect . '/claveunica/login/' . json_decode($response)->access_token);
+        } else {
+            return $this->getUserInfo(json_decode($response)->access_token);
+        }
+    }
 
-			$identifier = new Identifier();
-			$identifier->use = 'official';
-			$identifier->cod_con_identifier_type_id = 1; // RUN
-			$identifier->value = $user_clave_unica->RolUnico->numero;
-			$identifier->dv = $user_clave_unica->RolUnico->DV;
-			$identifier->user_id = $user_local->id;
-			$identifier->save();
+    public function getUserInfo($access_token)
+    {
+        /* Paso 3, obtener los datos del usuario en base al $access_token */
+        $url_base = "https://accounts.claveunica.gob.cl/openid/userinfo/";
+        $response = Http::withToken($access_token)->post($url_base);
 
-			$human_name = new HumanName();
-			$human_name->use = 'official';
-			$human_name->text = implode(' ', $user_clave_unica->name->nombres);
-			$human_name->fathers_family = $user_clave_unica->name->apellidos[0];
-			$human_name->mothers_family = $user_clave_unica->name->apellidos[1];
-			$human_name->user_id = $user_local->id;
-			$human_name->save();
+        $user_clave_unica = json_decode($response);
 
-			if(property_exists($user_clave_unica,'email')) { 
-				$contact_point = new ContactPoint();
-				$contact_point->system = 'email'; 
-				$contact_point->use = 'home';
-				$contact_point->value = $user_clave_unica->email; 
-				$contact_point->user_id = $user_local->id;
-				$contact_point->save();
-			}
-		}
+        /* Registrar los datos del usuario en la BD local */
 
-		Auth::login($user_local, true);
-		
-		return redirect()->route('home');
-		
-			
-		/* CU Entrega los datos del usuario en este formato
+        $user_local = User::query()
+            ->getByRun($user_clave_unica->RolUnico->numero)
+            ->first();
+
+        if ($user_local) {
+            /* Actualiza el correo si es que ha cambiado */
+            if (property_exists($user_clave_unica, 'email')) {
+                if ($user_local->email != $user_clave_unica->email) {
+                    $user_local->email = $user_clave_unica->email;
+                    $user_local->save();
+                }
+            }
+        } else {
+            $user_local = new User();
+            $user_local->active = 1;
+            $user_local->claveunica = true;
+            $user_local->save();
+
+            $identifier = new Identifier();
+            $identifier->use = 'official';
+            $identifier->cod_con_identifier_type_id = 1; // RUN
+            $identifier->value = $user_clave_unica->RolUnico->numero;
+            $identifier->dv = $user_clave_unica->RolUnico->DV;
+            $identifier->user_id = $user_local->id;
+            $identifier->save();
+
+            $human_name = new HumanName();
+            $human_name->use = 'official';
+            $human_name->text = implode(' ', $user_clave_unica->name->nombres);
+            $human_name->fathers_family = $user_clave_unica->name->apellidos[0];
+            $human_name->mothers_family = $user_clave_unica->name->apellidos[1];
+            $human_name->user_id = $user_local->id;
+            $human_name->save();
+
+            if (property_exists($user_clave_unica, 'email')) {
+                $contact_point = new ContactPoint();
+                $contact_point->system = 'email';
+                $contact_point->use = 'home';
+                $contact_point->value = $user_clave_unica->email;
+                $contact_point->user_id = $user_local->id;
+                $contact_point->save();
+            }
+        }
+
+        Auth::login($user_local, true);
+
+        return redirect()->route('home');
+
+
+        /* CU Entrega los datos del usuario en este formato
 		[RolUnico] => stdClass Object
 			(
 				[DV] => 4
@@ -170,25 +177,22 @@ class ClaveUnicaController extends Controller
 			)
 		[email] => mcdla@mail.com
 		*/
-	}
+    }
 
-	public function logout() {
-		/* Nos iremos al cerrar sesión en clave única y luego volvermos a nuestro sistema */
-		if(env('APP_ENV') == 'local')
-		{
-			/* Si es ambiente de desarrollo cerramos sólo localmente */
-			return redirect()->route('logout');
-		}
-		else
-		{
-			/** Cerrar sesión clave única */
-			/* Url para cerrar sesión en clave única */
-			$url_logout     = "https://accounts.claveunica.gob.cl/api/v1/accounts/app/logout?redirect=";
-			/* Url para luego cerrar sesión en nuestro sisetema */
-			$url_redirect   = env('APP_URL')."/logout";
-			$url            = $url_logout.urlencode($url_redirect);
-			return redirect($url);
-		}
-	}
-
+    public function logout()
+    {
+        /* Nos iremos al cerrar sesión en clave única y luego volvermos a nuestro sistema */
+        if (env('APP_ENV') == 'local') {
+            /* Si es ambiente de desarrollo cerramos sólo localmente */
+            return redirect()->route('logout');
+        } else {
+            /** Cerrar sesión clave única */
+            /* Url para cerrar sesión en clave única */
+            $url_logout     = "https://accounts.claveunica.gob.cl/api/v1/accounts/app/logout?redirect=";
+            /* Url para luego cerrar sesión en nuestro sisetema */
+            $url_redirect   = env('APP_URL') . "/logout";
+            $url            = $url_logout . urlencode($url_redirect);
+            return redirect($url);
+        }
+    }
 }
