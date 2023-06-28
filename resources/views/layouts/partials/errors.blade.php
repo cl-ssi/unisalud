d510ee57bccccc4d5d907b692d30f5585b00d15e
AquaroTorres <alvarotorres@gmail.com>
2023-06-28 19:03:03 -0300

    Fix error message on welcome

--- resources/views/layouts/partials/errors.blade.php
+++ resources/views/layouts/partials/errors.blade.php
@@ -1,6 +1,5 @@
 @if($errors->any())
-    <div class="alert alert-danger alert-dismissable mt-2 float-right" role="alert">
-        <a href="#" class="close" data-dismiss="alert" aria-label="close">&times;</a>
+    <div class="alert alert-danger" role="alert">
         <ul>
         @foreach($errors->all() as $error)
             <li>{{$error}}</li>
