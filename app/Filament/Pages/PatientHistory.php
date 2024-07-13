a6eebcd6f2aacd0da1b6e348f56e4a0f4b2896cf
Ignacio Miranda <piranga8@gmail.com>
2024-07-13 00:53:30 -0400

    MX: Avance PatientHistory - instalacion sushi

--- app/Filament/Pages/PatientHistory.php
+++ app/Filament/Pages/PatientHistory.php
@@ -7,6 +7,8 @@ use App\Models\Establishment;
 use App\Models\Exam;
 use App\Models\Patient;
 
+// use \Sushi\Sushi;
+
 use Filament\Pages\Page;
 
 use Filament\Forms;
@@ -14,26 +16,37 @@ use Filament\Forms\Form;
 use Filament\Forms\Contracts\HasForms;
 use Filament\Forms\Concerns\InteractsWithForms;
 
+/*
+use Filament\Tables;
+use Filament\Tables\Concerns\InteractsWithTable;
+use Filament\Tables\Contracts\HasTable;
+use Filament\Tables\Table;
+*/
+
 use Filament\Infolists;
 use Filament\Infolists\Infolist;
 
-use Illuminate\Support\Facades\DB;
-
-class PatientHistory extends Page implements HasForms
+class PatientHistory extends Page implements HasForms //, HasTable
 {
     use InteractsWithForms;
+    // use InteractsWithTable;
+    // use \Sushi\Sushi;
 
     protected static ?string $navigationIcon = 'heroicon-o-document-text';
 
     protected static string $view = 'filament.pages.patient-history';
 
-    // protected $patient;
+    // protected static bool $isLazy = false;
 
-    // public $rut;
+    public $patientHistory;
 
-    // public ?array $data = [];
+    public $rut;
 
+    public $commune;
 
+    public $original_establishment;
+
+    public $exam_establishment;
 
     public function form(Form $form): Form
     {
@@ -77,13 +90,14 @@ class PatientHistory extends Page implements HasForms
                     Forms\Components\Actions\Action::make('Buscar')
                         ->action(function (Forms\Get $get, Forms\Set $set) {
                             $run = str($get('rut'));
-                            // dd($this->form->getState());
-                            dd($run);
+                            list($run,$dv) = array_pad(explode('-',str_replace(".", "", $run)),2,null);
+                            // dd($run);
                             $code_deis_request = str($get('original_establishment'))?'':str($get('original_establishment'));
                             $code_deis = str($get('exam_establishment'))?'':str($get('exam_establishment'));
                             $commune = str($get('commune'))?'':str($get('commune'));
 
-                            $data = Exam::select(
+
+                            $this->patientHistory = Exam::select(
                                 'mx_patients.run',
                                 'mx_patients.dv',
                                 'mx_patients.name',
@@ -114,48 +128,8 @@ class PatientHistory extends Page implements HasForms
                             // ->where('mx_exams.cesfam', '=', $code_deis_request)
                             // ->where('mx_exams.comuna', '=', $commune)
                             ->get();
-                            // dd($data);
-                            // return $data;
-
-                            /*
-                            $sql="
-                                SELECT
-                                    T1.run,
-                                    T1.dv,
-                                    T1.name,
-                                    T1.fathers_family,
-                                    T1.mothers_family,
-                                    T1.gender,
-                                    T1.telephone,
-                                    DATE_FORMAT(T1.birthday, '%d/%m/%Y') AS birthday,
-                                    YEAR(CURDATE())-YEAR(T1.birthday) AS age,
-                                    T1.address,
-                                    DATE_FORMAT(T0.date_exam_order, '%d/%m/%Y') AS date_exam_order,
-                                    DATE_FORMAT(T0.date_exam, '%d/%m/%Y') AS date_exam,
-                                    DATE_FORMAT(T0.date_exam_reception, '%d/%m/%Y') AS date_exam_reception,
-                                    T0.birards_mamografia,
-                                    T0.birards_ecografia,
-                                    T0.birards_proyeccion,
-                                    T0.diagnostico,
-                                    ES2.alias AS establecimiento_realiza_examen,
-                                    T0.profesional_solicita,
-                                    T0.medico,
-                                    T0.servicio_salud,
-                                    CO.name AS comuna_name,
-                                    ES.alias AS cesfam_name
-                                FROM exams T0
-                                LEFT JOIN patients T1 ON T0.patient_id = T1.id
-                                LEFT JOIN communes CO  ON T0.comuna = CO.code_deis
-                                LEFT JOIN establishments ES  ON T0.cesfam = ES.new_code_deis
-                                LEFT JOIN establishments ES2  ON T0.establecimiento_realiza_examen = ES2.new_code_deis
-                                WHERE T1.run = '".$run."'
-                                    ".$code_deis_request ."
-                                    ".$code_deis ."
-                                    ".$commune."
-                                ORDER BY T0.id DESC";
-
-                            $patient = DB::select($sql);
-                            */
+                            // return $patientHistory;
+                            // dd($this->patientHistory);
 
                         }),
                     Forms\Components\Actions\Action::make('Limpiar')
@@ -168,18 +142,70 @@ class PatientHistory extends Page implements HasForms
             // ->statePath('data');
     }
 
-
     /*
-    public static function infolist(Infolist $infolist): Infolist
+    public function table(Table $table): Table
     {
-        return $infolist
-            ->schema([
-                Infolists\Components\TextEntry::make('run')
-                ->label('RUT'),
-                Infolists\Components\TextEntry::make('dv'),
+        // $temp['patientHistory'] = $this->patientHistory->toArray();
+        // $table->state($temp);
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('servicio_salud'),
+                Tables\Columns\TextColumn::make('cesfam_name'),
+                Tables\Columns\TextColumn::make('profesional_solicita'),
+                Tables\Columns\TextColumn::make('run'),
+                Tables\Columns\TextColumn::make('name'),
+                Tables\Columns\TextColumn::make('birthday'),
+                Tables\Columns\TextColumn::make('age'),
+                Tables\Columns\TextColumn::make('address'),
+                Tables\Columns\TextColumn::make('establecimiento_realiza_examen'),
+                Tables\Columns\TextColumn::make('date_exam_order'),
+                Tables\Columns\TextColumn::make('date_exam'),
+                Tables\Columns\TextColumn::make('date_exam_reception'),
+                Tables\Columns\TextColumn::make('birards_mamografia'),
+                Tables\Columns\TextColumn::make('birards_ecografia'),
+                Tables\Columns\TextColumn::make('birards_proyeccion'),
+                Tables\Columns\TextColumn::make('medico'),
             ]);
     }
     */
 
+
+    public function infolist(Infolist $infolist): Infolist
+    {
+        if($this->patientHistory == null){
+            return $infolist;
+        }
+        else {
+
+
+            // $infolist->record($this->patientHistory);
+            // $infolist->state('')
+            $temp['patientHistory'] = $this->patientHistory->toArray();
+            $infolist->state($temp);
+            // dd($this->patientHistory->toArray());
+            return $infolist
+                ->schema([
+                    Infolists\Components\RepeatableEntry::make('patientHistory')
+                        ->schema([
+                            Infolists\Components\TextEntry::make('servicio_salud'),
+                            Infolists\Components\TextEntry::make('cesfam_name'),
+                            Infolists\Components\TextEntry::make('profesional_solicita'),
+                            Infolists\Components\TextEntry::make('run'),
+                            Infolists\Components\TextEntry::make('name'),
+                            Infolists\Components\TextEntry::make('birthday'),
+                            Infolists\Components\TextEntry::make('age'),
+                            Infolists\Components\TextEntry::make('address'),
+                            Infolists\Components\TextEntry::make('establecimiento_realiza_examen'),
+                            Infolists\Components\TextEntry::make('date_exam_order'),
+                            Infolists\Components\TextEntry::make('date_exam'),
+                            Infolists\Components\TextEntry::make('date_exam_reception'),
+                            Infolists\Components\TextEntry::make('birards_mamografia'),
+                            Infolists\Components\TextEntry::make('birards_ecografia'),
+                            Infolists\Components\TextEntry::make('birards_proyeccion'),
+                            Infolists\Components\TextEntry::make('medico'),
+                        ])
+                ]);
+        }
+    }
 
 }
