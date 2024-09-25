e061d3d5d20b27f3fb8e1113574aad113875b446
Ignacio Miranda <piranga8@gmail.com>
2024-09-25 12:48:09 -0300

    Fix: Migraciones faltantes.

---
+++ app/Models/DependentCareGiver.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Model;
+
+use App\Models\DependentUser;
+
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasOne;
+
+class DependentCareGiver extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+    */
+    protected $fillable = [
+        'id',
+        'dependent_user_id',
+        'user_id',
+        'relative',
+        'empam',
+        'zarit',
+        'immunizations',
+        'elaborated_plan',
+        'evaluated_plan',
+        'trained',
+        'stipend',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function dependentUser(): HasOne
+    {
+        return $this->hasOne(DependentUser::class);
+    }
+
+    protected $table = 'dependent_caregiver';
+}
