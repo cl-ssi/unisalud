00393b16b66fbdfa4f10ddb4b8d930485a7d1998
Alvaro Torres F. <alvarotorres@gmail.com>
2024-09-26 14:07:47 -0300

    Welcome widget update generico

--- resources/views/filament/widgets/welcome-widget.blade.php
+++ resources/views/filament/widgets/welcome-widget.blade.php
@@ -1,29 +1,24 @@
 <x-filament-widgets::widget>
     <x-filament::section>
+        <p class="font-bold text-xl">
+            {{ env('APP_SS') }}
+        </p>
+        <br>
         <p style="text-align: justify;">
-            El Servicio de Salud Tarapacá un organismo estatal, dependiente de la Subsecretaría de Redes Asistenciales,
+            Es un organismo estatal, dependiente de la Subsecretaría de Redes Asistenciales,
             funcionalmente descentralizado, dotado con personalidad jurídica y patrimonio propio. Está sometido a la
             supervigilancia del Ministerio de Salud para el cumplimiento de las políticas, normas y planes generales que
             éste determine.<br>
 
-            Fue creado mediante el Decreto Ley Nº 2763 del 3 de agosto de 1979, que dio origen al Sistema Nacional de
-            Servicios de Salud, en reemplazo del anterior Sistema Nacional de Salud, primer ordenamiento de las
-            instituciones sanitarias del país que estaba vigente desde 1952.<br>
-
-            El Servicio de Salud Tarapacá es uno de los veintinueve Servicios de Salud del país, de carácter autónomo
-            que tiene la responsabilidad de velar por la salud de los habitantes de la región de Tarapacá. Los
-            establecimientos públicos de atención en salud de la I región se encuentran divididos según su complejidad y
+            <br>
+            El {{ env('APP_SS') }} es uno de los veintinueve Servicios de Salud del país, de carácter autónomo
+            que tiene la responsabilidad de velar por la salud de los habitantes de la región. <br>
+            
+            <br>
+            Los establecimientos públicos de atención en salud se encuentran divididos según su complejidad y
             dependencia administrativa, los de administración municipal, referidos principalmente a la atención de nivel
-            primario, y los dependientes del Servicio de Salud Iquique, referidos a los niveles de mediana a mayor
-            complejidad y resolutividad.<br>
-
-            La Red Asistencial cuenta con una totalidad de 38 establecimientos de salud, de los cuales el 89 % se
-            encuentran bajo la administración municipal y el 11% del Servicio Salud Tarapacá.<br>
-
-            La distribución de establecimientos por comuna se encuentra principalmente concentrada en los asentamientos
-            urbanizados, especialmente en la Comuna de Iquique con el 30%, seguidos por Alto Hospicio con un 19% y Pozo
-            Almonte con un 14%, las comunas rurales de Huara cuentan con un 16%, Pica 8% y Colchane con un 8 % y Camiña
-            con un 5%.
+            primario (CESFAM, CECOSF, Postas Rurales y SAPU), y los dependientes del Servicio de Salud, 
+            referidos a los niveles de mediana a mayor complejidad y resolutividad (Hospitales, COSAM y SAMU).<br>
         </p>
     </x-filament::section>
 </x-filament-widgets::widget>
