e114f936165f27bc652d5c554949b63c94c8b61a
Ignacio Miranda <piranga8@gmail.com>
2025-08-14 13:19:20 -0400

    Modificar colores disponibles

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Event;
 use Illuminate\Support\ServiceProvider;
 use Filament\Http\Responses\Auth\Contracts\LogoutResponse as LogoutResponseContract;
 use Illuminate\Support\Facades\Gate;
+use Filament\Support\Colors\Color;
+use Filament\Support\Facades\FilamentColor;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -30,9 +32,40 @@ class AppServiceProvider extends ServiceProvider
         /** Para que los Policies tengan la misma estructura de los modelos */
         Gate::guessPolicyNamesUsing(function ($modelClass) {
             // Reemplaza 'App\Models' por 'App\Policies' y añade 'Policy' al final del nombre de la clase
-            $policyClass = str_replace('App\Models', 'App\Policies', $modelClass).'Policy';
+            $policyClass = str_replace('App\Models', 'App\Policies', $modelClass) . 'Policy';
 
             return $policyClass;
         });
+
+        FilamentColor::register([
+            'danger' => Color::Red,
+            'gray' => Color::Zinc,
+            'info' => Color::Blue,
+            'primary' => Color::Indigo,
+            'success' => Color::Green,
+            'warning' => Color::Amber,
+            // Custom Colors
+            'red' => Color::Red,
+            'orange' => Color::Orange,
+            'amber' => Color::Amber,
+            'yellow' => Color::Yellow,
+            'lime' => Color::Lime,
+            'green' => Color::Green,
+            'emerald' => Color::Emerald,
+            'teal' => Color::Teal,
+            'cyan' => Color::Cyan,
+            'sky' => Color::Sky,
+            'blue' => Color::Blue,
+            'indigo' => Color::Indigo,
+            'violet' => Color::Violet,
+            'purple' => Color::Purple,
+            'fuchsia' => Color::Fuchsia,
+            'pink' => Color::Pink,
+            'rose' => Color::Rose,
+            'slate' => Color::Slate,
+            'zinc' => Color::Zinc,
+            'neutral' => Color::Neutral,
+            'stone' => Color::Stone,
+        ]);
     }
 }
